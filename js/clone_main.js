9d64aa058deb66e228a2f9637895d14794394fc3
sunnny27 <chltjs_8@naver.com>
2023-05-06 00:20:53 +0900

    헤로피 커피사이트 클론코딩 1

--- js/clone_main.js
+++ js/clone_main.js
@@ -1,8 +1,21 @@
 // 검색창 요소찾기 (.search)
 // 검색요소 searchEl 선언, 입력요소 searchinputEl 선언
-
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
 
 // 검색창 요소를 클릭하면 실행
 searchEl.addEventListener('click', function(){
-  searchinputEl.focus();
+  searchInputEl.focus();
+});
+
+// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
+searchInputEl.addEventListener('focus', function(){
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder','통합검색');
+});
+
+// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
+searchInputEl.addEventListener('blur',function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder','');
 });
