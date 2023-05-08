69183a9a08b60d6a2cdb93ebb1771032be5637bd
sunnny27 <chltjs_8@naver.com>
2023-05-08 23:59:12 +0900

    헤로피 커피사이트 수정

--- js/clone_main.js
+++ js/clone_main.js
@@ -1,3 +1,23 @@
+//상단 팝업
+function slideUp() {
+  var box = document.getElementById("box");
+  var height = box.clientHeight;
+  var animation = box.animate(
+    [
+      { height: height + "px" },
+      { height: "0px" },
+    ],
+    {
+      duration: 500,
+      easing: "ease-out",
+    }
+  );
+  animation.onfinish = function () {
+    box.style.display = "none";
+  };
+}
+
+
 // 검색창 요소찾기 (.search)
 // 검색요소 searchEl 선언, 입력요소 searchinputEl 선언
 const searchEl = document.querySelector('.search');
@@ -69,39 +89,39 @@ fadeEls.forEach(function (fadeEl, index) {
 
 /* 슬라이드 요소 */
 new Swiper('.notice .swiper', {
-  direction: 'vertical', 
-  autoplay: true,
-  loop: true 
+  direction: 'vertical', // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부
 });
-
 new Swiper('.promotion .swiper', {
-  autoplay: { 
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: { // 자동 재생 여부
     delay: 5000 // 5초마다 슬라이드 바뀜
   },
   loop: true, // 반복 재생 여부
-  slidesPerView: 3,
-  spaceBetween: 10,
-  centeredSlides: true,
-  pagination: { 
-    el: '.promotion .swiper-pagination', 
-    clickable: true 
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  pagination: { // 페이지 번호 사용
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소
+    clickable: true // 사용자의 페이지 번호 제어 여부
   },
-  navigation: {
-    prevEl: '.promotion .swiper-button-prev', 
-    nextEl: '.promotion .swiper-button-next'
+  navigation: { // 슬라이드 이전/다음 버튼 사용
+    prevEl: '.promotion .swiper-button-prev', // 이전 버튼 요소
+    nextEl: '.promotion .swiper-button-next' // 다음 버튼 요소
+  }
+});
+new Swiper('.awards .swiper', {
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+  spaceBetween: 30, // 슬라이드 사이 여백
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+  navigation: { // 슬라이드 이전/다음 버튼 사용
+    prevEl: '.awards .swiper-button-prev', // 이전 버튼 요소
+    nextEl: '.awards .swiper-button-next' // 다음 버튼 요소
   }
 });
 
-new Swiper('.awards .swiper', {
-  autoplay: true, 
-  loop: true, 
-  spaceBetween: 30, 
-  slidesPerView: 5,
-  navigation: { 
-    prevEl: '.awards .swiper-button-prev', 
-    nextEl: '.awards .swiper-button-next' 
-  }
-});
 
 /* 프로모션 슬라이드*/
 
@@ -151,3 +171,6 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show') 
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
